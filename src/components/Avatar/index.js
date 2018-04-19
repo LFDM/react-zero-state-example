de63e06914b30ad59d209507ec8825d97c007523
LFDM <1986gh@gmail.com>
2018-04-19 22:43:34 +0200

    feat(ui) Implement Avatar

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,1 +1,28 @@
-export default () => null;
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { keys } from 'lodash/fp';
+import styles from './style.less';
+
+const AVATAR_SIZES = {
+  XS: 'extraSmall',
+  SM: 'small',
+  MD: 'medium',
+  LG: 'large',
+  XL: 'xLarge'
+};
+
+Avatar.propTypes = {
+  user: PropTypes.object.isRequired,
+  size: PropTypes.oneOf(keys(AVATAR_SIZES)),
+  className: PropTypes.string,
+};
+
+Avatar.defaultProps = {
+  size: 'MD',
+};
+
+export default function Avatar({ user, size, className }) {
+  const sizeClass = AVATAR_SIZES[size];
+  const alt = `Avatar for ${user.name}`;
+  return <img src={user.avatar} className={classNames(styles.round, styles[sizeClass], className)} alt={alt} />;
+}
