e264aa360fb34bd1a0441de66d0a0dbb42fa7d29
LFDM <1986gh@gmail.com>
2018-04-19 16:25:05 +0200

    feat(praise) Setup page

--- src/features/Praise/index.js
+++ src/features/Praise/index.js
@@ -1,5 +1,14 @@
+import { Page, PageTitle, PageBody } from '../../layout/Page';
+import List from '../../features/Praise/List';
+import RecipientFilter from '../../features/Praise/RecipientFilter';
+
 export default function Praise() {
   return (
-    <div>Praise</div>
+    <Page>
+      <PageTitle>Praise</PageTitle>
+      <PageBody>
+        <List />
+      </PageBody>
+    </Page>
   );
 }
