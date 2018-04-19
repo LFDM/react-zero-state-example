c56efa057efd4204e7e3e9bb87df56ea778d418c
LFDM <1986gh@gmail.com>
2018-04-19 19:17:58 +0200

    fix(modal) Fix modal problems

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -3,6 +3,8 @@ import ReactModal from 'react-modal';
 import classNames from 'classnames';
 import { noop } from 'lodash/fp';
 
+import styles from './style.less';
+
 const TRANSITION_DURATION = 150;
 
 const SIZES = {
@@ -18,6 +20,7 @@ const SIZES = {
 const parseMinHeight = minHeight =>
   typeof minHeight === 'number' ? `${minHeight}px` : minHeight || 'auto';
 
+const asSizeValue = v => (typeof v === 'number' ? `${v}px` : v);
 
 const ModalTitle = ({ children, className }) => (
   <h1 className={classNames(styles.title, className)}>
@@ -25,33 +28,28 @@ const ModalTitle = ({ children, className }) => (
   </h1>
 );
 
-export default ({ title, size, width, minHeight, isOpen, onClose }) => {
+export default ({ title, size, width, minHeight, isOpen, onClose, children }) => {
   const style = {
     overlay: {
       transitionDuration: TRANSITION_DURATION + 'ms',
     },
     content: {
-      width: width || asSizeValue(SIZES[props.size]),
+      width: width || asSizeValue(SIZES[size]),
       minHeight: parseMinHeight(minHeight),
     },
   };
-
-  const content = (
-  );
 
   const props = {
     isOpen,
     onRequestClose: onClose || noop,
     closeTimeoutMS: TRANSITION_DURATION,
     style,
-    children: (
-    ),
     className: styles.content,
     overlayClassName: styles.overlay
   };
 
   return (
-    <ReactModal {...props}>
+    <ReactModal {...props} ariaHideApp={false}>
       <Fragment>
         {title && <ModalTitle>{title}</ModalTitle>}
         {children}
