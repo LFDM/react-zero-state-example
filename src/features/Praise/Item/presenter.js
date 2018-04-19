820c1c9486ffa65027d484e210a9dedba9d4875f
LFDM <1986gh@gmail.com>
2018-04-19 21:17:29 +0200

    feat(praise) Add dropdown handlers

--- src/features/Praise/Item/presenter.js
+++ src/features/Praise/Item/presenter.js
@@ -1,12 +1,37 @@
+import { Fragment } from 'react';
 import { find } from 'lodash/fp';
 import { Dropdown, OptionWithModal } from '../../../components/Dropdown';
-import { ButtonLink } from '../../../components/Button';
+import { ButtonContainer, ButtonSecondary, ButtonDangerous, ButtonLink } from '../../../components/Button';
 import AvatarListStacked from '../../../components/AvatarListStacked';
 import PromiseTracker, { STATUS, forStatus } from '../../../components/PromiseTracker';
+import Form from '../Form';
 import styles from './style.less'
 
 
 const hasLiked = (user, likes) => !!find(u => user.id === u.id, likes);
+
+const RemoveConfirmation = ({ onCancel, onConfirm }) => (
+
+  <PromiseTracker onTrigger={onConfirm}>
+    {({ trigger, status }) =>
+      <Fragment>
+        <p>This process cannot be reverted! </p>
+        <ButtonContainer>
+          <ButtonSecondary
+            disabled={status !== STATUS.IDLE}
+            onClick={onCancel}>
+            Cancel
+          </ButtonSecondary>
+
+          { status === STATUS.IDLE ?
+            <ButtonDangerous onClick={trigger}>Delete</ButtonDangerous> :
+            <ButtonSecondary disabled={true}>Deleting...</ButtonSecondary>
+          }
+        </ButtonContainer>
+      </Fragment>
+    }
+  </PromiseTracker>
+);
 
 const LikeButton = ({ hasLiked, like, unlike }) => (
   <PromiseTracker onTrigger={hasLiked ? unlike : like}>
@@ -20,7 +45,7 @@ const LikeButton = ({ hasLiked, like, unlike }) => (
 );
 
 export default ({
-  praise: { id, title, body, author, recipient, likes },
+  praise,
   currentUser,
   edit,
   remove,
@@ -30,27 +55,53 @@ export default ({
   <div>
     <div className={styles.container}>
       <div className={styles.recipient}>
-        <img className={styles.recipientAvatar} src={recipient.avatar} />
-        <div className={styles.recipientName}>{recipient.name}</div>
+        <img className={styles.recipientAvatar} src={praise.recipient.avatar} />
+        <div className={styles.recipientName}>{praise.recipient.name}</div>
       </div>
 
       <div className={styles.body}>
         <div className={styles.content}>
           <div className={styles.title}>
-            <h2>{title}</h2>
+            <h2>{praise.title}</h2>
             <Dropdown>
-              <OptionWithModal>Edit</OptionWithModal>
-              <OptionWithModal>Delete</OptionWithModal>
+              <OptionWithModal
+                modalProps={{
+                  title: 'Edit Praise',
+                  size: 'M_WIDTH',
+                  body: ({ closeModal }) =>
+                    <Form
+                      praise={praise}
+                      label="Edit"
+                      onCancel={closeModal}
+                      onSubmit={p => edit({ ...praise, ...p })}
+                    />
+                }}
+              >
+                Edit
+              </OptionWithModal>
+              <OptionWithModal
+                modalProps={{
+                  title: 'Are you sure?',
+                  size: 'S_WIDTH',
+                  body: ({ closeModal }) =>
+                    <RemoveConfirmation
+                      onConfirm={() => remove(praise.id)}
+                      onCancel={closeModal}
+                    />
+                }}
+              >
+                Delete
+              </OptionWithModal>
             </Dropdown>
           </div>
-          <div>{body}</div>
+          <div>{praise.body}</div>
         </div>
       </div>
 
       <div className={styles.footer}>
-        <AvatarListStacked users={likes} />
+        <AvatarListStacked users={praise.likes} />
         <LikeButton
-          hasLiked={hasLiked(currentUser, likes)}
+          hasLiked={hasLiked(currentUser, praise.likes)}
           like={like}
           unlike={unlike}
         />
