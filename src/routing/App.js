88987cbacacea6bc77b32324d857c35036e5c7ef
LFDM <1986gh@gmail.com>
2018-04-19 23:19:54 +0200

    feat(style) Start main style

--- src/routing/App.js
+++ src/routing/App.js
@@ -1,4 +1,4 @@
-import { Route, Redirect, Router, Switch } from 'react-router';
+import { Route, Router, Switch } from 'react-router';
 import * as R from './routes';
 import RouteError from './RouteError';
 import history from './history';
@@ -8,17 +8,21 @@ import { ContextProvider } from '../context';
 import Home from '../features/Home';
 import Praise from '../features/Praise';
 
+import styles from './style.less';
+
 export default function App() {
   return (
-    <ContextProvider>
-       <Router history={history}>
-         <Switch>
-           <Route exact path={R.HOME} render={() => <Home />} />
-           <Route exact path={R.PRAISE} render={() => <Praise />} />
-           <Route render={() => <RouteError errorCode={404} />} />
-         </Switch>
-       </Router>
-    </ContextProvider>
+    <div className={styles.main}>
+      <ContextProvider>
+         <Router history={history}>
+           <Switch>
+             <Route exact path={R.HOME} render={() => <Home />} />
+             <Route exact path={R.PRAISE} render={() => <Praise />} />
+             <Route render={() => <RouteError errorCode={404} />} />
+           </Switch>
+         </Router>
+      </ContextProvider>
+    </div>
   );
 }
 
