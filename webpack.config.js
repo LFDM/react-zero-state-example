71d1c8bd6d94d67ed2b243f031287813dd46c51f
LFDM <1986gh@gmail.com>
2018-04-19 08:04:30 +0200

    chore(eslint) Add setup

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: ['babel-loader', 'eslint-loader']
         }
       },
       {
