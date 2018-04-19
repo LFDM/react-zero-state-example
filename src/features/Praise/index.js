008645388cc8bd6f56cc415861d2340e80ab8b95
LFDM <1986gh@gmail.com>
2018-04-19 19:18:41 +0200

    feat(praise) Add Create button

--- src/features/Praise/index.js
+++ src/features/Praise/index.js
@@ -1,12 +1,14 @@
 import { Page, PageTitle, PageBody } from '../../layout/Page';
 import List from '../../features/Praise/List';
 import RecipientFilter from '../../features/Praise/RecipientFilter';
+import CreateButton from '../../features/Praise/CreateButton';
 
 export default function Praise() {
   return (
     <Page>
       <PageTitle>Praise</PageTitle>
       <PageBody>
+        <CreateButton />
         <List />
       </PageBody>
     </Page>
