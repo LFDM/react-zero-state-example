f6eb83285e081bb6707f224bba640e9948475c4c
LFDM <1986gh@gmail.com>
2018-04-19 16:25:43 +0200

    feat(layout) Start with layout components

---
+++ src/layout/Page/index.js
@@ -0,0 +1,3 @@
+export const Page = ({ children }) => <div>{children}</div>;
+export const PageTitle = ({ children }) => <h1>{children}</h1>;
+export const PageBody = ({ children }) => <div>{children}</div>;
