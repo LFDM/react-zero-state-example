df3be2d16d9cf4a9c65d470fbfa031e40cac3e9b
LFDM <1986gh@gmail.com>
2018-04-20 06:59:03 +0200

    feat(ui) Improve button factory

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,23 +1,23 @@
 import classNames from 'classnames';
 import styles from './style.less';
 
-const buttonFactory = (className, type = 'button') => (props) => (
+const factory = (className, type = 'button') => (props) => (
   <button
     {...props}
     type={props.type || type}
-    className={classNames(styles.button, className, props.className)}
+    className={classNames(className, props.className)}
   />
 );
 
-
-
+const buttonFactory = (className, type = 'button') =>
+  factory(classNames(styles.button, className, type));
 
 export const ButtonSubmit = buttonFactory(styles.primary, 'submit');
 export const ButtonPrimary = buttonFactory(styles.primary);
 export const ButtonSecondary =  buttonFactory(styles.secondary);
 export const ButtonDangerous =  buttonFactory(styles.dangerous);
-export const ButtonInline = (props) => <button type="button" { ...props } />;
-export const ButtonLink = buttonFactory(styles.link);
+export const ButtonInline = factory();
+export const ButtonLink = factory(styles.link);
 
 export const ButtonContainer = ({ className, children }) => <div className={classNames(styles.container, className)}>{children}</div>;
 
