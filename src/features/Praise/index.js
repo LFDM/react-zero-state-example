50d193f50783dcac7405cc70aeae571a31c6ec24
LFDM <1986gh@gmail.com>
2018-04-20 08:17:18 +0200

    feat(praise) Deal with filter param

--- src/features/Praise/index.js
+++ src/features/Praise/index.js
@@ -3,19 +3,19 @@ import List from '../../features/Praise/List';
 import RecipientFilter from '../../features/Praise/RecipientFilter';
 import CreateButton from '../../features/Praise/CreateButton';
 
-export default function Praise() {
+export default function Praise({ recipientId, updateRecipientId }) {
   return (
     <Page>
       <PageHeader>
         <PageTitle>Praise</PageTitle>
         <PageControls>
-          <RecipientFilter />
+          <RecipientFilter recipientId={recipientId} onUpdate={updateRecipientId}/>
           <CreateButton />
         </PageControls>
       </PageHeader>
 
       <PageBody>
-        <List />
+        <List recipientId={recipientId}/>
       </PageBody>
     </Page>
   );
--- src/routing/App.js
+++ src/routing/App.js
@@ -2,6 +2,7 @@ import { Route, Router, Switch } from 'react-router';
 import * as R from './routes';
 import RouteError from './RouteError';
 import history from './history';
+import queryString from 'query-string';
 
 import { ContextProvider } from '../context';
 
@@ -17,7 +18,12 @@ export default function App() {
          <Router history={history}>
            <Switch>
              <Route exact path={R.HOME} render={() => <Home />} />
-             <Route exact path={R.PRAISE} render={() => <Praise />} />
+             <Route exact path={R.PRAISE} render={({ location, history}) => (
+               <Praise
+                 recipientId={queryString.parse(location.search).recipientId}
+                 updateRecipientId={recipientId => history.replace({ search: toString({ recipientId}) })}
+               />
+             )} />
              <Route render={() => <RouteError errorCode={404} />} />
            </Switch>
          </Router>
