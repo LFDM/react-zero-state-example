f06133037ec486ed6b37a152eaa2bf94793bf20d
LFDM <1986gh@gmail.com>
2018-04-20 09:52:52 +0200

    feat(praise) Show who you're filtering

--- src/features/Praise/index.js
+++ src/features/Praise/index.js
@@ -1,13 +1,23 @@
+import { withData } from 'ladda-react';
+import api from '../../api';
 import { Page, PageTitle, PageHeader, PageControls, PageBody } from '../../layout/Page';
 import List from '../../features/Praise/List';
 import RecipientFilter from '../../features/Praise/RecipientFilter';
 import CreateButton from '../../features/Praise/CreateButton';
 
+const TitleSuffix = withData({
+  resolve: {
+    user: ({ recipientId }) => recipientId ? api.user.getById(recipientId) : Promise.resolve(null)
+  }
+})(({ user }) => user ? <span>for {user.name}</span> : null);
+
 export default function Praise({ recipientId, updateRecipientId }) {
   return (
     <Page>
       <PageHeader>
-        <PageTitle>Praise</PageTitle>
+        <PageTitle>
+          Praise <TitleSuffix recipientId={recipientId} />
+        </PageTitle>
         <PageControls>
           <RecipientFilter recipientId={recipientId} onUpdate={updateRecipientId}/>
           <CreateButton />
