1265a1e9736b37efab427cef730d121b00dda404
LFDM <1986gh@gmail.com>
2018-04-20 10:30:22 +0200

    fix(praise) Fix typo in RecipientFilter

--- src/features/Praise/RecipientFilter/index.js
+++ src/features/Praise/RecipientFilter/index.js
@@ -1,5 +1,5 @@
 import AutocompleteUser from '../../../components/AutocompleteUser';
 
 export default ({ recipientId, onUpdate }) => (
-  <AutocompleteUser placeholder="Filter by recipient" onSelect={c => onUpdate(c.d)} selectedUserIds={[recipientId]} />
+  <AutocompleteUser placeholder="Filter by recipient" onSelect={c => onUpdate(c.id)} selectedUserIds={[recipientId]} />
 );
