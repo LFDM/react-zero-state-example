257e0c4b5abf57c0f3b15f8a88e14e749f592d52
LFDM <1986gh@gmail.com>
2018-04-20 09:56:07 +0200

    feat(praise) Allow to clear filter

--- src/features/Praise/index.js
+++ src/features/Praise/index.js
@@ -1,22 +1,26 @@
 import { withData } from 'ladda-react';
+import { IconX } from 'featherico'
 import api from '../../api';
 import { Page, PageTitle, PageHeader, PageControls, PageBody } from '../../layout/Page';
 import List from '../../features/Praise/List';
 import RecipientFilter from '../../features/Praise/RecipientFilter';
 import CreateButton from '../../features/Praise/CreateButton';
+import { ButtonInline } from '../../components/Button';
 
 const TitleSuffix = withData({
   resolve: {
     user: ({ recipientId }) => recipientId ? api.user.getById(recipientId) : Promise.resolve(null)
   }
-})(({ user }) => user ? <span>for {user.name}</span> : null);
+})(({ user, onClear }) => user ? (
+  <span>for {user.name} <ButtonInline onClick={onClear}><IconX /></ButtonInline></span>
+) : null);
 
 export default function Praise({ recipientId, updateRecipientId }) {
   return (
     <Page>
       <PageHeader>
         <PageTitle>
-          Praise <TitleSuffix recipientId={recipientId} />
+          Praise <TitleSuffix recipientId={recipientId} onClear={() => updateRecipientId(undefined)}/>
         </PageTitle>
         <PageControls>
           <RecipientFilter recipientId={recipientId} onUpdate={updateRecipientId}/>
