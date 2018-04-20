ce31974349766047726c4583de35268a43d2204b
LFDM <1986gh@gmail.com>
2018-04-20 07:47:56 +0200

    feat(praise) Update page layout

--- src/features/Praise/index.js
+++ src/features/Praise/index.js
@@ -1,4 +1,4 @@
-import { Page, PageTitle, PageBody } from '../../layout/Page';
+import { Page, PageTitle, PageHeader, PageControls, PageBody } from '../../layout/Page';
 import List from '../../features/Praise/List';
 import RecipientFilter from '../../features/Praise/RecipientFilter';
 import CreateButton from '../../features/Praise/CreateButton';
@@ -6,9 +6,15 @@ import CreateButton from '../../features/Praise/CreateButton';
 export default function Praise() {
   return (
     <Page>
-      <PageTitle>Praise</PageTitle>
+      <PageHeader>
+        <PageTitle>Praise</PageTitle>
+        <PageControls>
+          <RecipientFilter />
+          <CreateButton />
+        </PageControls>
+      </PageHeader>
+
       <PageBody>
-        <CreateButton />
         <List />
       </PageBody>
     </Page>
