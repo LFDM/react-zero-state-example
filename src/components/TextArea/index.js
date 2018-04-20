189bb49414d9bb82e105c7b4c3581455c482b5b2
LFDM <1986gh@gmail.com>
2018-04-20 16:07:29 +0200

    feat(ui) Style TextArea

--- src/components/TextArea/index.js
+++ src/components/TextArea/index.js
@@ -1,1 +1,5 @@
-export default (props) => <textarea { ...props } />;
+import classNames from 'classnames';
+import styles from './style.less';
+
+export default ({ className, ...props }) =>
+  <textarea className={classNames(styles.textArea, className)} { ...props } />;
